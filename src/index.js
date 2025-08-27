d9f4bd9c17bc78cb48e5cc7a9baed3dd1d6984ea
Silversocks <muhammedshamil.kvn@gmail.com>
2025-08-27 21:55:19 +0530

    almot good commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const express=require("express");
+const db=require('./routes/db');
+
 port=8080;
 
 const app=express();
+
+app.post("/initialiseapp",(req,res)=>{
+    db.query("insert into books values")
+    res.json()
+})
+
+app.post("")
 
 app.listen(prompt,console.log("started server"));
